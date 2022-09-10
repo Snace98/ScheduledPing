5b49f9b7ec76937413314e3ff1b1539483d9fa1d
Spence Nace <spence.nace@outlook.com>
2022-09-10 14:33:52 -0500

    Start moving data into dict

--- ScheduledPing.py
+++ ScheduledPing.py
@@ -2,24 +2,46 @@ import os
 from datetime import datetime, timedelta
 from time import sleep
 
-class ScheduledPing:
-    pingData = [{}]
-
+pingData = {
+    'packets': [],
+    'minimum': [],
+    'maximum': [],
+    'average': []
+}
 
 def runPingCheck():
     runTimer = float(input("Run job for (in minutes): "))
     startTime = datetime.today()
     while datetime.today() < (startTime + timedelta(minutes=runTimer)):
         os.system('ping 208.67.222.222 >> pingData.txt')
-        convertPingData()
-        sleep(1)
+        sleep(5)
+    convertPingData()
 
 def convertPingData():
     file = open('pingData.txt')
     for line in file:
-        os.system('echo ' + line)
         if "Packets: " in line:
-         os.system('echo ' + line)
+         pingData['packets'].append(line)
+        if "Minimum" in line:
+            minMaxAvg(line)
+
+    os.system('echo ' + debugHelper('packets', ''))
+    os.system('echo ' + debugHelper('minimum', ''))
+    os.system('echo ' + debugHelper('maximum', ''))
+    os.system('echo ' + debugHelper('average', ''))
     return
+
+def minMaxAvg(line):
+    minMaxAvgArr = line.split(',')
+    pingData['minimum'].append(minMaxAvgArr[0])
+    pingData['maximum'].append(minMaxAvgArr[1])
+    pingData['average'].append(minMaxAvgArr[2])
+
+    
+
+def debugHelper(prop, myString):
+    for x in pingData[prop]:
+        myString += x
+    return myString
 
 runPingCheck()
